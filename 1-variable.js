90c45a7f9175d5d3e30aa68eeb81c1720fe2ec7a
Aisha Gench <aisha.gench@gmail.com>
2021-05-20 14:49:57 -0500

    create operators

--- 1-variable.js
+++ 1-variable.js
@@ -1,1 +1,22 @@
 console.log('Hello World');
+
+var quantity;
+quantity = 5;
+
+console,console.log(quantity);
+var i = 100;
+i = 10;
+console.log(i);
+
+var x =5, y = 'String';
+console.log(x + y);
+
+
+var a;
+console.log(a) //undefined
+
+console.log(typeof x);
+
+//prompt operator
+var name = prompt('Enter your name');
+console.log(namme);
---
+++ 2-operators.js
@@ -0,0 +1,23 @@
+//Arithmetic
+console.log(3 + 5 * 4 - 6);
+
+//Assignment
+var x = 5;
+x+=5;
+console.log(x);
+
+//Comparison
+
+var one  = 1;
+var one_again = 1;
+var one_string = '1';
+
+console.log(one  == one_again); //true
+console.log(one  === one_again); //true
+console.log(one == one_string); //true //same value even types are different
+console.log(one === one_string); //false //types are different
+
+//Logical
+console.log((5<2) && (2>=3));
+console.log((5<2) || (2>=3));
+console.log(!(5==3));
