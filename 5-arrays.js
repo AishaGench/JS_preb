8e12d50112847464d8664bde87fa260ae199fb67
Aisha Gench <aisha.gench@gmail.com>
2021-05-21 12:30:19 -0500

    remove element from the beginning

---
+++ 5-arrays.js
@@ -0,0 +1,26 @@
+//Creating array
+var scores = new Array(); //empty
+var numbers = new Array(10); //array with initial size 10
+var myNumbers = new Array(9,10,4,5); //array with initial elements
+
+var athletes = Array(3); //array with initial size 3
+var signs = Array('Red'); //array with one elements
+
+var emptyarray = [];
+var colors = ['red','green','blue'];
+
+//Accesing array elements
+var students = ['Aisha', 'Dennis', 'Sevde','Beyza']
+console.log(students);
+console.log(students.toString());
+console.log(students[0]);
+
+//Basic operations on arrays
+students.push('Betul'); //adds element to end of an Array
+console.log(students);
+
+students.unshift('Nurgul'); //adds element to beginning of an Array
+console.log(students);
+
+students.pop(); //removes an element from the end of an arrays
+console.log(students);
