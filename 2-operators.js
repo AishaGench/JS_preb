0b6242c79c0154df2e3ad67d54584ea084c9be78
Aisha Gench <aisha.gench@gmail.com>
2021-05-23 12:57:25 -0500

    add some diffirent task

--- 2-operators.js
+++ 2-operators.js
@@ -15,7 +15,7 @@ a = -a-- + a++ / -a-- * --a;
 console.log(a);
 
 var x = 4;
-var y =  x++;
+var y = x * 4 - x++;
 console.log(y);
 
 //Comparison
@@ -32,3 +32,33 @@ console.log(one === one_string); //false //types are different
 console.log((5<2) && (2>=3));
 console.log((5<2) || (2>=3));
 console.log(!(5==3));
+
+var num = 12;
+var num2 =5;
+ if(num > num2){
+   console.log("Firts number " + num + " is greater than second number " + num2 );
+ }else{
+   console.log("Second number " + num2 + " is greater than first number " + num);
+ }
+
+var applesCount = 20;
+var orangesCount = 30;
+var pearsCount = 30;
+var comp = applesCount < orangesCount || orangesCount >= pearsCount;
+console.log(comp);
+
+var outsideWeather;
+var degree;
+outsideWeather = "Shinny";
+degree = 70;
+var comp2 = (!(outsideWeather == "Raining" || degree == 70));
+console.log(comp2);
+
+var b =2;
+var res = ++b == 2 || --b == 2 && --b == 2;
+console.log(res);
+
+var x = true, z = true;
+var y = 20;
+x =(y != 10) || (z = false);
+console.log(x);
