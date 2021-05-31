10cae9d94c145658c48badf440e4b5ca84cb2671
Aisha Gench <aisha.gench@gmail.com>
2021-05-30 23:16:12 -0500

    create split method

---
+++ 7-string.js
@@ -0,0 +1,14 @@
+var str = 'Java Script';
+console.log(str.length);
+console.log(str.charAt(2));
+console.log(str.includes('Java'));
+
+var str2 = 'Hello Aysegul, welcome to JavaScript world!'
+console.log(str2.indexOf('JavaScript'));
+console.log(str.indexOf('JavaScript',27));
+
+var replaced =str2.replace('JavaScript', 'React');
+console.log(replaced);
+
+var arr = str2.split('');
+console.log(arr);
