91197f6f2b220db23d96d2ea4705ba014a324392
Aisha Gench <aisha.gench@gmail.com>
2021-05-30 23:28:34 -0500

    use substring method

--- 7-string.js
+++ 7-string.js
@@ -10,5 +10,9 @@ console.log(str.indexOf('JavaScript',27));
 var replaced =str2.replace('JavaScript', 'React');
 console.log(replaced);
 
-var arr = str2.split('');
+var arr = str2.split(' '); //if you put one space inside of the parentheses it writes the word by one by with quotation
 console.log(arr);
+
+console.log(str2.substring(0,5));
+console.log(str2.substring(6,10)); //(from, to)
+console.log(str2.substr(6,4));  //(from, length)
