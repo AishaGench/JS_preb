b1975392db27f31ce8418afcf835c42f5107a311
Aisha Gench <aisha.gench@gmail.com>
2021-06-07 13:52:20 -0500

    create variable with var let and const

---
+++ 9-es6-variable.js
@@ -0,0 +1,17 @@
+var x = 1;  //var key word global and function scoped so console write 2 / 2
+if(x===1){
+  var x  =2;
+  console.log(x);
+}
+console.log(x);
+
+let y =1;      //  let is block scoped
+if(y===1){
+  let y = 2;
+  console.log(y);
+}
+console.log(y);
+
+const num = 50; //it is constant variable doesnt change
+//num = 40;
+console.log(num); // it gives to an error (typeError: assigment to constant variable)
