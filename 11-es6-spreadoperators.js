f3d0f96f22abc751643f0c25bf01e94bb35d3d27
Aisha Gench <aisha.gench@gmail.com>
2021-06-15 00:09:40 -0700

    use map inside of the spreadoperators(...)

--- 11-es6-spreadoperators.js
+++ 11-es6-spreadoperators.js
@@ -5,3 +5,18 @@ console.log(arr);
 let str = 'Hello';
 var chars = [...str];
 console.log(chars);
+
+//Rest operators collects all elements into array
+function fun(...theArgs){
+  console.log(theArgs.length);
+}
+fun();
+fun(5);
+fun(5,6,7)
+
+function multiply(multiplier,...theArgs){
+  return theArgs.map(element => multiplier * element);
+}
+
+let arry = multiply(2,15,25,42);
+console.log(arry);
